a7903c47e6af39ffd879519deea4ceb46777d47f
JaclynSmith <60448949+JaclynSmith@users.noreply.github.com>
2024-08-31 11:29:38 -0400

    Update define_read_consensus.py

--- VSG-AMP-Seq/define_read_consensus.py
+++ VSG-AMP-Seq/define_read_consensus.py
@@ -1,6 +1,6 @@
 """AMP Seq: This module creates mosaic consensus sequences from overlapping reads."""
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
-import primer_sort
+import aux_functions
 import Levenshtein as lev
 import regex
 import glob
@@ -341,7 +341,7 @@ def unknown_overlap(seq1, seq2, qual1, qual2, primer):
 
 def consensus_builder():
     # get target primer ranges
-    primer_dict, primer_seq_dict = primer_sort.read_in_primers("antat_primers.txt")
+    primer_dict, primer_seq_dict = aux_functions.read_in_primers("antat_primers.txt")
     target_ranges = target_primer_id(primer_dict)
 
     # output variable initialization
